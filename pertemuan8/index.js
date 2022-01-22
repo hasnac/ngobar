ab4570a9159d79c99691390b56a70db40db5daf3
hasnac <93810603+hasnac@users.noreply.github.com>
2022-01-22 11:24:12 +0700

    Add files via upload

---
+++ pertemuan8/index.js
@@ -0,0 +1,11 @@
+document.getElementById('btn').addEventListener('click', function() {
+    let nama = document.getElementById('nama').value
+    alert('Hi, ' + nama)
+    let kelas = document.getElementById('kelas').value
+    alert('Apakah kamu kelas ' + kelas + '?')
+    let alamt = document.getElementById('alamt').value
+    alert('Apakah kamu masuk ' + alamt +'?')
+    let tentang = document.getElementById('tentang').value
+    alert('Kamu pasti ' + tentang)
+    
+})
